7faf6cd032707392fb4a68ded2ef53a962744d3f
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-03-14 13:40:44 +0200

    Update for ARS changes

--- releasemaker/step/updates.php
+++ releasemaker/step/updates.php
@@ -3,17 +3,17 @@
  * Akeeba Release Maker
  * An automated script to upload and release a new version of an Akeeba component.
  * Copyright ©2012-2013 Nicholas K. Dionysopoulos / Akeeba Ltd.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,22 +24,22 @@ class ArmStepUpdates implements ArmStepInterface
 	{
 		echo "PUSHING UPDATE INFORMATION\n";
 		echo str_repeat('-', 79) . PHP_EOL;
-		
+
 		echo "\tPushing Core updates\n";
 		$this->deployUpdates('core');
-		
+
 		echo "\tPushing Pro updates\n";
 		$this->deployUpdates('pro');
-		
+
 		echo PHP_EOL;
 	}
-	
+
 	private function deployUpdates($prefix = 'core')
 	{
 		$conf = ArmConfiguration::getInstance();
-		
+
 		$type = $conf->get('common.update.method', 'sftp');
-		
+
 		if($type == 's3') {
 			$config = (object)array(
 				'access'		=> $conf->get('common.update.s3.access', ''),
@@ -60,50 +60,53 @@ class ArmStepUpdates implements ArmStepInterface
 				'directory'		=> $conf->get('common.update.ftp.directory', ''),
 			);
 		}
-		
+
 		$stream_id = $conf->get($prefix . '.update.stream', 0);
 		$formats = $conf->get($prefix . '.update.formats', array());
 		$basename = $conf->get($prefix . '.update.basename', '');
 		$url = $conf->get('common.arsapiurl', '');
-		
+
 		// No base name means that no updates are set here
 		if(empty($basename))
 		{
 			return;
 		}
-		
+
 		$tempPath = realpath(__DIR__ . '/../tmp/');
-		
+
 		foreach ($formats as $format_raw)
 		{
 			echo "\t\tPushing $format_raw update format over $type\n";
-			
+
 			switch ($format_raw)
 			{
 				case 'ini':
 					$extension = '.ini';
 					$format = 'ini';
+					$task = '';
 					break;
-				
+
 				case 'inibare':
 					$extension = '';
 					$format = 'ini';
+					$task = '';
 					break;
-				
+
 				case 'xml':
 					$extension = '.xml';
 					$format = 'xml';
+					$task = '&task=stream';
 					break;
 			}
-			
+
 			$temp_filename = $tempPath . '/' . $basename . $extension;
-			
-			$updateURL = $url . "/index.php?option=com_ars&view=update&format=$format&id=$stream_id";
-			
+
+			$updateURL = $url . "/index.php?option=com_ars&view=update&task=stream&format=$format&id=$stream_id" . $task;
+
 			$data = file_get_contents($updateURL);
 			//var_dump($updateURL, $data);die();
 			file_put_contents($temp_filename, $data);
-			
+
 			switch($type) {
 				case 's3':
 					$this->uploadS3($config, $temp_filename);
@@ -116,7 +119,7 @@ class ArmStepUpdates implements ArmStepInterface
 					$this->uploadSftp($config, $temp_filename);
 					break;
 			}
-			
+
 			unlink($temp_filename);
 		}
 	}
@@ -124,7 +127,7 @@ class ArmStepUpdates implements ArmStepInterface
 	private function uploadS3($config, $sourcePath, $destName = null)
 	{
 		$s3 = ArmAmazonS3::getInstance($config->access, $config->secret, $config->usessl);
-		
+
 		$inputFile = realpath($sourcePath);
 		$bucket = $config->bucket;
 		if(empty($destName)) {
@@ -143,23 +146,23 @@ class ArmStepUpdates implements ArmStepInterface
 		$input = ArmAmazonS3::inputFile($inputFile, true);
 		$result = $s3->putObject($input, $bucket, $uri, $acl, array(), $requestHeaders);
 	}
-	
+
 	private function uploadFtp($config, $sourcePath, $destName = null)
 	{
 		if(empty($destName)) {
 			$destName = basename($sourcePath);
 		}
-		
+
 		$ftp = new ArmFtp($config);
 		$ftp->upload($sourcePath, $destName);
 	}
-	
+
 	private function uploadSftp($config, $sourcePath, $destName = null)
 	{
 		if(empty($destName)) {
 			$destName = basename($sourcePath);
 		}
-		
+
 		$sftp = new ArmSftp($config);
 		$sftp->upload($sourcePath, $destName);
 	}
