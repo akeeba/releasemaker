a9b45bc97d0b81f99cf7a70d88068ed3f242434e
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-01-02 18:08:02 +0200

    Changing copyright headers. Happy New Year!

--- releasemaker/utils/string.php
+++ releasemaker/utils/string.php
@@ -2,7 +2,7 @@
 /**
  * Akeeba Release Maker
  * An automated script to upload and release a new version of an Akeeba component.
- * Copyright ©2012-2016 Nicholas K. Dionysopoulos / Akeeba Ltd.
+ * Copyright ©2012-2017 Nicholas K. Dionysopoulos / Akeeba Ltd.
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
