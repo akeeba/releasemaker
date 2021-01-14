e3b9e09cb4655251d025e15030acdec5e5eec76b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-01-14 13:57:16 +0200

    Refactoring with Rector
    Set up Rector

---
+++ rector.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * @package    AkeebaReleaseMaker
+ * @copyright  Copyright (c)2012-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license    GNU General Public License version 3, or later
+ */
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+	// Required to prevent early death for classes references the S3 library
+	if (!defined('AKEEBAENGINE'))
+	{
+		define('AKEEBAENGINE', 1);
+	}
+
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+
+	// Use PHP 7.2 features
+	$parameters->set(Option::PHP_VERSION_FEATURES, '7.2');
+
+	// Paths to include
+	$parameters->set(Option::PATHS, [
+		__DIR__ . '/src',
+	]);
+
+	/**
+	 * Paths to exclude.
+	 */
+	$parameters->set(Option::EXCLUDE_PATHS, [
+		__DIR__ . '/src/cacert.pem',
+		__DIR__ . '/src/config.json',
+		__DIR__ . '/composer.json',
+		__DIR__ . '/composer.lock',
+		__DIR__ . '/releasemaker/*',
+		__DIR__ . '/tmp/*',
+		__DIR__ . '/vendor/*',
+	]);
+
+	/**
+	 * Rectors to exclude.
+	 */
+	$parameters->set(Option::EXCLUDE_RECTORS, [
+		// This Rector seems to die consistently in Rector 0.8
+		//\Rector\CodeQuality\Rector\Isset_\IssetOnPropertyObjectToPropertyExistsRector::class,
+
+		// WATCH OUT! This does crazy things, like convert $ret['ErrorException'] to $ret[\ErrorException::class] which
+		// is unfortunate and messes everything up.
+		//\Rector\Php55\Rector\String_\StringClassNameToClassConstantRector::class,
+
+		//\Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector::class,
+	]);
+
+	// Define what rule sets will be applied
+    $parameters->set(Option::SETS, [
+        SetList::EARLY_RETURN,
+//	    SetList::CODE_QUALITY,
+//	    SetList::CODE_QUALITY_STRICT,
+//	    SetList::CODING_STYLE,
+//	    SetList::DEAD_CLASSES,
+//	    SetList::DEAD_CODE,
+//	    SetList::DEAD_DOC_BLOCK,
+//	    SetList::GMAGICK_TO_IMAGICK,
+//	    SetList::NAMING,
+//	    SetList::ORDER,
+//	    SetList::PERFORMANCE,
+//	    SetList::PHP_52,
+//	    SetList::PHP_53,
+//	    SetList::PHP_54,
+//	    SetList::PHP_55,
+//	    SetList::PHP_56,
+//	    SetList::PHP_70,
+//	    SetList::PHP_71,
+//	    SetList::PHP_72,
+//	    SetList::PRIVATIZATION,
+//	    SetList::PSR_4,
+//	    SetList::UNWRAP_COMPAT,
+    ]);
+
+    // get services (needed for register a single rule)
+    // $services = $containerConfigurator->services();
+
+    // register a single rule
+    // $services->set(TypedPropertyRector::class);
+};
